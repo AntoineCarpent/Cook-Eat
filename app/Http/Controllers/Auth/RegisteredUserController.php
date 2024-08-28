88315175ee3ed67fa1ea3bf9d6ac68535ff2aa68
Sophie Théréau <sophiethereau@hotmail.fr>
2024-08-28 15:33:06 +0200

    User

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -22,12 +22,14 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'role' => ['required', 'string'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'name' => $request->username,
+            'role' => $request->role,
             'email' => $request->email,
             'password' => Hash::make($request->string('password')),
         ]);
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::get('/', function () {
+    return 'Hello World';
 });
+
+Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
+
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+    
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');  
+
+});
+
+
