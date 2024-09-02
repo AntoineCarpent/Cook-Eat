5780670f1366876b497d50e804ebd55d009bd37a
Sophie Théréau <sophiethereau@hotmail.fr>
2024-09-02 09:46:45 +0200

    routes dans le middleware

--- routes/api.php
+++ routes/api.php
@@ -12,21 +12,7 @@ use App\Http\Controllers\FavoriteController;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 
-
 });
-
-
-
-    Route::get('/recipes', [RecipeController::class, 'index']); 
-    Route::get('/recipes/{id}', [RecipeController::class, 'show']); 
-    Route::post('/recipes', [RecipeController::class, 'store']); 
-    Route::put('/recipes/{id}', [RecipeController::class, 'update']); 
-    Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
-    Route::get('/ingredients', [IngredientController::class, 'index']); 
-    Route::get('/ingredients/{id}', [IngredientController::class, 'show']); 
-    Route::post('/ingredients', [IngredientController::class, 'store']); 
-    Route::put('/ingredients/{id}', [IngredientController::class, 'update']);
-    Route::delete('/ingredients/{id}', [IngredientController::class, 'destroy']); 
 
 
 Route::get('/', function () {
@@ -41,6 +27,7 @@ Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::get('/isadmin', [UserController::class, 'isAdmin'])->name('users.isadmin');
+
     
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
@@ -49,9 +36,32 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
     
     Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy'); 
+
     
     Route::post('/favorites', [FavoriteController::class, 'store'])->name('favorites.store');
     
     Route::delete('/favorites', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
 
+    
+    Route::get('/recipes', [RecipeController::class, 'index']); 
+
+    Route::get('/recipes/{id}', [RecipeController::class, 'show']); 
+
+    Route::post('/recipes', [RecipeController::class, 'store']); 
+
+    Route::put('/recipes/{id}', [RecipeController::class, 'update']); 
+
+    Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
+
+
+    Route::get('/ingredients', [IngredientController::class, 'index']); 
+
+    Route::get('/ingredients/{id}', [IngredientController::class, 'show']); 
+
+    Route::post('/ingredients', [IngredientController::class, 'store']); 
+
+    Route::put('/ingredients/{id}', [IngredientController::class, 'update']);
+
+    Route::delete('/ingredients/{id}', [IngredientController::class, 'destroy']); 
+    
 });
